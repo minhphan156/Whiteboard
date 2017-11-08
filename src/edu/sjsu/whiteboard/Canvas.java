457b0004f968fcf2145fee3baf7d8942ef815cc6
Danil Kolesnikov <danil.kolesnikov@sjsu.edu>
2017-11-07 17:40:15 -0800

    First commit

---
+++ src/edu/sjsu/whiteboard/Canvas.java
@@ -0,0 +1,9 @@
+package edu.sjsu.whiteboard;
+
+import javax.swing.*;
+
+/**
+ * Created by danil on 11/7/17.
+ */
+public class Canvas extends JPanel {
+}
---
+++ src/edu/sjsu/whiteboard/Whiteboard.java
@@ -0,0 +1,10 @@
+package edu.sjsu.whiteboard;
+
+import javax.swing.*;
+
+public class Whiteboard extends JFrame {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
