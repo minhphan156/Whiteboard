7824ba7cd703a9b61b5c24b1a6859c185234898c
Danil Kolesnikov <danil.kolesnikov@sjsu.edu>
2017-11-11 12:33:37 -0800

    Dummy commit

--- src/edu/sjsu/whiteboard/Canvas.java
+++ src/edu/sjsu/whiteboard/Canvas.java
@@ -1,9 +1,12 @@
 package edu.sjsu.whiteboard;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by danil on 11/7/17.
  */
 public class Canvas extends JPanel {
+    for (Component comp : box.getComponents()) { ((JComponent)comp).setAlignmentX(Box.LEFT_ALIGNMENT);
+    }
 }
