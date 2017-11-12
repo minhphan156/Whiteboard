2dddcd6d1d4901a26be913199ae4a76753644e2a
Danil Kolesnikov <danil.kolesnikov@sjsu.edu>
2017-11-11 17:04:41 -0800

    UI Impoved

--- src/edu/sjsu/whiteboard/InterfaceControl.java
+++ src/edu/sjsu/whiteboard/InterfaceControl.java
@@ -2,6 +2,8 @@ package edu.sjsu.whiteboard;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by danil on 11/11/17.
@@ -25,12 +27,37 @@ public class InterfaceControl extends JPanel {
         shapesHorizontalBox.add(text);
 
         Box setColorHorizontalBox = Box.createHorizontalBox();
-        JButton scriptChooser = new JButton("Script");
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        JComboBox scriptChooser = new JComboBox(fonts);
+        scriptChooser.addActionListener (new ActionListener () {
+            public void actionPerformed(ActionEvent e) {
+                String selectedValue = scriptChooser.getSelectedItem().toString();
+                System.out.print("\nUser selected: "+selectedValue);
+
+            }
+        });
         JButton setColor = new JButton("Set Color");
+        JButton colorButton = new JButton("");
+        colorButton.setBackground(Color.GRAY);
+        colorButton.setOpaque(true);
+        colorButton.setBorderPainted(false);
+
+        ActionListener actionListener = new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent) {
+                Color initialBackground = setColor.getBackground();
+                Color color = JColorChooser.showDialog(null, "JColorChooser", initialBackground);
+                colorButton.setBackground(color);
+                colorButton.setOpaque(true);
+                colorButton.setBorderPainted(false);
+
+            }
+        };
+        setColor.addActionListener(actionListener);
         JTextField textField = new JTextField("");
         setColorHorizontalBox.add(textField);
         setColorHorizontalBox.add(scriptChooser);
         setColorHorizontalBox.add(setColor);
+        setColorHorizontalBox.add(colorButton);
 
         Box toolsHBox = Box.createHorizontalBox();
         JButton moveToFront = new JButton("Move to Front");
@@ -40,10 +67,28 @@ public class InterfaceControl extends JPanel {
         toolsHBox.add(moveToBack);
         toolsHBox.add(removeShape);
 
+        String[] columns = new String[] {
+                "X", "Y", "Width", "Height"
+        };
+
+        Object[][] data = new Object[][] {
+                {10, 10, 111, 58 },
+                {56, 52, 221, 56 },
+                {18, 148, 361, 120 },
+        };
+        Box tableHBox = Box.createHorizontalBox();
+        JTable table = new JTable(data, columns);
+        tableHBox.add(table);
+        JScrollPane sp = new JScrollPane(table);
+
         verticalBoxMain.add(shapesHorizontalBox);
         verticalBoxMain.add(setColorHorizontalBox);
         verticalBoxMain.add(toolsHBox);
+        verticalBoxMain.add(tableHBox);
+        for (Component comp : verticalBoxMain.getComponents()) { ((JComponent)comp).setAlignmentX(Box.CENTER_ALIGNMENT);
+        }
         this.add(verticalBoxMain);
+        this.add(sp);
 
     }
 
