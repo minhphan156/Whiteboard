b28419418b0886df2eba28ffccc88104014750aa
Minh Phan <minhphan156@gmail.com>
2017-12-04 09:10:59 -0800

    brought back menu items

--- src/edu/sjsu/whiteboard/Whiteboard.java
+++ src/edu/sjsu/whiteboard/Whiteboard.java
@@ -25,19 +25,150 @@ public class Whiteboard extends JFrame {
 
     public Whiteboard(Controller controller){
         super("Whiteboard");
-        canvas =  new Canvas(controller);
+         canvas =  new Canvas(controller);
+        JMenuBar menubar = new JMenuBar(); // Initialize the menubar
+
+        // Icons
+        ImageIcon iconNew = new ImageIcon("res/reset.png");
+        ImageIcon iconOpen = new ImageIcon("res/open.png");
+        ImageIcon iconSave = new ImageIcon("res/save.png");
+        ImageIcon iconSavePNG = new ImageIcon("res/save.png");
+        ImageIcon iconExit = new ImageIcon("exit.png");
+        ImageIcon iconAuthors = new ImageIcon("res/info.png");
+        ImageIcon iconAuthorsDialog = new ImageIcon("res/infoDialog.png");
+
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        JMenu editMenu = new JMenu("Edit");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        JMenu networkingMenu = new JMenu("Networking");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+
+        JMenuItem moveToFront = new JMenuItem(new MenuItemAction("Move to Front", null,
+                KeyEvent.VK_N));
+
+        moveToFront.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                canvas.moveToFront();
+            }
+        });
+
+        JMenuItem moveToBack = new JMenuItem(new MenuItemAction("Move to Back", null,
+                KeyEvent.VK_N));
+
+        moveToBack.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                canvas.moveToBack();
+            }
+        });
+        JMenuItem removeShape = new JMenuItem(new MenuItemAction("Remove Shape", null,
+                KeyEvent.VK_N));
+        removeShape.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                canvas.deleteShape();
+            }
+        });
+
+        // Add controls to editMenu
+        editMenu.add(moveToFront);
+        editMenu.add(moveToBack);
+        editMenu.addSeparator();
+        editMenu.add(removeShape);
+
+        JMenuItem startServ = new JMenuItem(new MenuItemAction("Start Server", null,
+                KeyEvent.VK_N));
+        JMenuItem startClient = new JMenuItem(new MenuItemAction("Start Client", null,
+                KeyEvent.VK_N));
+
+        // Add sub menus to networking menu
+        networkingMenu.add(startServ);
+        networkingMenu.add(startClient);
+
+
+        JMenu aboutMenu = new JMenu("About");
+        JMenuItem authorsMI = new JMenuItem(new MenuItemAction("Authors", iconAuthors,
+                KeyEvent.VK_N));
+        aboutMenu.add(authorsMI);
+
+        authorsMI.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(authorsMI,"SJSU CS 151 Term Project - Whiteboard"+"\nDanil Kolesnikov danil.kolesnikov@sjsu.edu" +
+                        "\nMinh Phan minh.phan@sjsu.edu" +
+                        "\nYulan Jin yulan.jin@sjsu.edu","Authors",1,iconAuthorsDialog);
+            }
+        });
+
+
+        JMenuItem newMi = new JMenuItem(new MenuItemAction("Reset", iconNew,
+                KeyEvent.VK_N));
+
+        // Reset the canvas
+        newMi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                canvas.clearCanvas();
+            }
+        });
+
+        JMenuItem openMi = new JMenuItem(new MenuItemAction("Open", iconOpen,
+                KeyEvent.VK_O));
+        JMenuItem saveMi = new JMenuItem(new MenuItemAction("Save", iconSave,
+                KeyEvent.VK_S));
+        JMenuItem savePNG = new JMenuItem(new MenuItemAction("Save PNG", iconSavePNG,
+                KeyEvent.VK_S));
+
+        // Exit button
+        JMenuItem exitMi = new JMenuItem("Exit", iconExit);
+        exitMi.setMnemonic(KeyEvent.VK_E);
+        exitMi.setToolTipText("Exit whiteboard");
+        exitMi.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W,
+                ActionEvent.CTRL_MASK));
+
+        exitMi.addActionListener((ActionEvent event) -> {
+            System.exit(0);
+        });
+
+        // Add sub menus in the File menu
+        fileMenu.add(newMi);
+        fileMenu.add(openMi);
+        fileMenu.add(saveMi);
+        fileMenu.add(savePNG);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMi);
+
+        // Add 4 sub menus in the menu bar
+        menubar.add(fileMenu);
+        menubar.add(editMenu);
+        menubar.add(networkingMenu);
+        menubar.add(aboutMenu);
+
         this.controller = controller;
         this.setLayout(new BorderLayout());
         this.add(canvas, BorderLayout.CENTER);
         this.add(new InterfaceControl(controller,canvas), BorderLayout.WEST);
-        //this.add(menubar,BorderLayout.NORTH);
+        this.add(menubar,BorderLayout.NORTH);
         this.pack();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setVisible(true);
     }
 
-    public Canvas getCanvas() {
-        return canvas;
+    // Some helper function
+    private class MenuItemAction extends AbstractAction {
+
+        public MenuItemAction(String text, ImageIcon icon, Integer mnemonic) {
+            super(text);
+            putValue(SMALL_ICON, icon);
+            putValue(MNEMONIC_KEY, mnemonic);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println(e.getActionCommand()); // Prints the name of the button pressed
+        }
     }
 
     // return either Canvas or InterfaceControl inside whiteboard
@@ -54,4 +185,7 @@ public class Whiteboard extends JFrame {
         return (JPanel)comp;
     }
 
+    public Canvas getCanvas() {
+        return canvas;
+    }
 }
